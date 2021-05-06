82c51dd62f4fc355b030c6b7e0d8a1b1bb6be3d7
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2021-05-06 22:08:18 +0500

    create gulpfile & add config for pug & scss process

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const { src, dest, parallel, series, watch } = require('gulp');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const del = require('del');
+const pug = require('gulp-pug');
+const scss = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleancss = require('gulp-clean-css');
+
+function buildHTML() {
+  return src('app/pages/**/*.pug')
+    .pipe(pug())
+    .pipe(rename({ extname: '.html' }))
+    .pipe(dest('dist'));
+}
+
+function buildStyles() {
+  return src('app/**/*.scss')
+    .pipe(scss())
+    .pipe(concat('styles.min.css'))
+    .pipe(autoprefixer())
+    .pipe(cleancss())
+    .pipe(dest('dist'));
+}
+
+function cleanDistFolder() {
+  return del('dist', { force: true });
+}
+
+exports.buildHTML = buildHTML;
+exports.buildStyles = buildStyles;
+
+exports.default = series(
+  cleanDistFolder,
+  parallel(
+    buildHTML,
+    buildStyles,
+  )
+);
